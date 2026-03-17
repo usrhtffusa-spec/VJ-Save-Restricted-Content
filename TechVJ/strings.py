59e90ad1f44426629507212788ddc1e7bd8b7df2
usrhtffusa-spec <usrhtffusa@gmail.com>
2026-03-17 11:34:28 +0530

    Update strings.py

--- TechVJ/strings.py
+++ TechVJ/strings.py
@@ -1,36 +1,15 @@
-# Don't Remove Credit Tg - @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Developer - @usrhtff009
+# Channel - https://t.me/usrht01
 
-HELP_TXT = """**🌟 Help Menu** 
+HELP_TXT = """🌟 Help Menu 
 
-**__FOR PRIVATE CHATS__**
+FOR PRIVATE CHATS
 
-__first send invite link of the chat (unnecessary if the account of string session already member of the chat)
-then send post/s link__
-
-
-**__FOR BOT CHATS__**
-
-__send link with '/b/', bot's username and message id, you might want to install some unofficial client (like - Plus Messenger) to get the id like below__
-
-```
-https://t.me/b/botusername/4321
-```
-
-**__MULTI POSTS__**
-
-__send public/private posts link as explained above with formate "from - to" to send multiple messages like below__
+First send invite link of the chat (unnecessary if the account of string session already member of the chat)
+then send post/s link
 
 
-```
-https://t.me/xxxx/1001-1010
+FOR BOT CHATS
 
-https://t.me/c/xxxx/101 - 120
-```
+Send link with '/b/', bot's username and message id. You might want to install some unofficial client (like - Plus Messenger) to get the id like below:
 
-__note that space in between doesn't matter__"""
-       
-# Don't Remove Credit Tg - @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
