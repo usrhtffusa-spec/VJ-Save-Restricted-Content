0a02bcc00f95a7278b8f72f07bde7eeb39fd3568
usrhtffusa-spec <usrhtffusa@gmail.com>
2026-03-17 11:36:40 +0530

    Update broadcast.py

--- TechVJ/broadcast.py
+++ TechVJ/broadcast.py
@@ -1,6 +1,5 @@
-# Don't Remove Credit Tg - @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Developer - @usrhtff009
+# Channel - https://t.me/usrht01
 
 from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid
 from database.db import db
@@ -29,13 +28,12 @@ async def broadcast_messages(user_id, message):
     except Exception as e:
         return False, "Error"
 
-
 @Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
 async def broadcast(bot, message):
     users = await db.get_all_users()
     b_msg = message.reply_to_message
     if not b_msg:
-        return await message.reply_text("**Reply This Command To Your Broadcast Message**")
+        return await message.reply_text("Reply This Command To Your Broadcast Message")
     sts = await message.reply_text(
         text='Broadcasting your messages...'
     )
@@ -68,10 +66,9 @@ async def broadcast(bot, message):
             failed += 1
             if not done % 20:
                 await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
-    
+
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
     await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
 
-# Don't Remove Credit @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Developer - @usrhtff009
+# Channel - https://t.me/usrht01
