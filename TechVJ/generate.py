f68c716c35d3c90873972552590a1ab1ced12649
usrhtffusa-spec <usrhtffusa@gmail.com>
2026-03-17 11:35:40 +0530

    Update generate.py

--- TechVJ/generate.py
+++ TechVJ/generate.py
@@ -1,6 +1,5 @@
-# Don't Remove Credit Tg - @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Developer - @usrhtff009
+# Channel - https://t.me/usrht01
 
 import traceback
 from pyrogram.types import Message
@@ -26,17 +25,19 @@ async def logout(client, message):
     if user_data is None:
         return 
     await db.set_session(message.from_user.id, session=None)  
-    await message.reply("**Logout Successfully** ♦")
+    await message.reply("Logout Successfully ♦")
 
 @Client.on_message(filters.private & ~filters.forwarded & filters.command(["login"]))
 async def main(bot: Client, message: Message):
     user_data = await db.get_session(message.from_user.id)
     if user_data is not None:
-        await message.reply("**Your Are Already Logged In. First /logout Your Old Session. Then Do Login.**")
+        await message.reply("Your Are Already Logged In. First /logout Your Old Session. Then Do Login.")
         return 
     user_id = int(message.from_user.id)
-    await message.reply("**How To Create Api Id And Api Hash.\n\nVideo Link :- https://youtu.be/LDtgwpI-N7M**")
-    api_id_msg = await bot.ask(user_id, "<b>Send Your API ID.\n\nClick On /skip To Skip This Process\n\nNOTE :- If You Skip This Then Your Account Ban Chance Is High.</b>", filters=filters.text)
+    await message.reply("How To Create Api Id And Api Hash.\n\nVideo Link :- https://youtu.be/LDtgwpI-N7M")
+    
+    api_id_msg = await bot.ask(user_id, "Send Your API ID.\n\nClick On /skip To Skip This Process\n\nNOTE :- If You Skip This Then Your Account Ban Chance Is High.", filters=filters.text)
+    
     if api_id_msg.text == "/skip":
         api_id = API_ID
         api_hash = API_HASH
@@ -44,49 +45,56 @@ async def main(bot: Client, message: Message):
         try:
             api_id = int(api_id_msg.text)
         except ValueError:
-            await api_id_msg.reply("**Api id must be an integer, start your process again by /login**", quote=True, reply_markup=InlineKeyboardMarkup(gen_button))
+            await api_id_msg.reply("Api id must be an integer, start your process again by /login", quote=True)
             return
-        api_hash_msg = await bot.ask(user_id, "**Now Send Me Your API HASH**", filters=filters.text)
+        api_hash_msg = await bot.ask(user_id, "Now Send Me Your API HASH", filters=filters.text)
         api_hash = api_hash_msg.text
-        
-    phone_number_msg = await bot.ask(chat_id=user_id, text="<b>Please send your phone number which includes country code</b>\n<b>Example:</b> <code>+13124562345, +9171828181889</code>")
-    if phone_number_msg.text=='/cancel':
-        return await phone_number_msg.reply('<b>process cancelled !</b>')
+
+    phone_number_msg = await bot.ask(chat_id=user_id, text="Please send your phone number which includes country code\nExample: +13124562345, +9171828181889")
+    if phone_number_msg.text == '/cancel':
+        return await phone_number_msg.reply('Process cancelled !')
+    
     phone_number = phone_number_msg.text
     client = Client(":memory:", api_id, api_hash)
     await client.connect()
     await phone_number_msg.reply("Sending OTP...")
+    
     try:
         code = await client.send_code(phone_number)
-        phone_code_msg = await bot.ask(user_id, "Please check for an OTP in official telegram account. If you got it, send OTP here after reading the below format. \n\nIf OTP is `12345`, **please send it as** `1 2 3 4 5`.\n\n**Enter /cancel to cancel The Procces**", filters=filters.text, timeout=600)
+        phone_code_msg = await bot.ask(user_id, "Please check for an OTP in official telegram account. If you got it, send OTP here after reading the below format. \n\nIf OTP is 12345, please send it as 1 2 3 4 5.\n\nEnter /cancel to cancel The Procces", filters=filters.text, timeout=600)
     except PhoneNumberInvalid:
-        await phone_number_msg.reply('`PHONE_NUMBER` **is invalid.**')
+        await phone_number_msg.reply('PHONE_NUMBER is invalid.')
         return
-    if phone_code_msg.text=='/cancel':
-        return await phone_code_msg.reply('<b>process cancelled !</b>')
+        
+    if phone_code_msg.text == '/cancel':
+        return await phone_code_msg.reply('Process cancelled !')
+        
     try:
         phone_code = phone_code_msg.text.replace(" ", "")
         await client.sign_in(phone_number, code.phone_code_hash, phone_code)
     except PhoneCodeInvalid:
-        await phone_code_msg.reply('**OTP is invalid.**')
+        await phone_code_msg.reply('OTP is invalid.')
         return
     except PhoneCodeExpired:
-        await phone_code_msg.reply('**OTP is expired.**')
+        await phone_code_msg.reply('OTP is expired.')
         return
     except SessionPasswordNeeded:
-        two_step_msg = await bot.ask(user_id, '**Your account has enabled two-step verification. Please provide the password.\n\nEnter /cancel to cancel The Procces**', filters=filters.text, timeout=300)
-        if two_step_msg.text=='/cancel':
-            return await two_step_msg.reply('<b>process cancelled !</b>')
+        two_step_msg = await bot.ask(user_id, 'Your account has enabled two-step verification. Please provide the password.\n\nEnter /cancel to cancel The Procces', filters=filters.text, timeout=300)
+        if two_step_msg.text == '/cancel':
+            return await two_step_msg.reply('Process cancelled !')
         try:
             password = two_step_msg.text
             await client.check_password(password=password)
         except PasswordHashInvalid:
-            await two_step_msg.reply('**Invalid Password Provided**')
+            await two_step_msg.reply('Invalid Password Provided')
             return
+            
     string_session = await client.export_session_string()
     await client.disconnect()
+    
     if len(string_session) < SESSION_STRING_SIZE:
-        return await message.reply('<b>invalid session sring</b>')
+        return await message.reply('Invalid session string')
+        
     try:
         user_data = await db.get_session(message.from_user.id)
         if user_data is None:
@@ -100,10 +108,9 @@ async def main(bot: Client, message: Message):
             except:
                 pass
     except Exception as e:
-        return await message.reply_text(f"<b>ERROR IN LOGIN:</b> `{e}`")
-    await bot.send_message(message.from_user.id, "<b>Account Login Successfully.\n\nIf You Get Any Error Related To AUTH KEY Then /logout first and /login again</b>")
+        return await message.reply_text(f"ERROR IN LOGIN: {e}")
+        
+    await bot.send_message(message.from_user.id, "Account Login Successfully.\n\nIf You Get Any Error Related To AUTH KEY Then /logout first and /login again")
 
-
-# Don't Remove Credit Tg - @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Developer - @usrhtff009
+# Channel - https://t.me/usrht01
